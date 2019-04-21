9cbd1141a922165899d8327ddf7e69ad17d7e0db
pl-xts <Blefor93@gmail.com>
2019-04-21 20:05:34 +0300

    update file name for storing results

--- tensorflow_hub/utils/store_results.py
+++ tensorflow_hub/utils/store_results.py
@@ -1,11 +1,11 @@
-path = './results.txt'
-file = open(path,'a+')
 
 
 def store_results(name, num_of_classes, avg_score, time, video_type):
+    path = './results_' + video_type + '.txt'
+    file = open(path,'a+')
     string = "{};{};{};{};{}\n".format(name, num_of_classes, round(avg_score, 2), time, video_type)
     file.write(string)
-
+    file.close()
 # store_results('resnet', 100, 55, "155", "night")
 # file.close()
 # reader = open(path, 'r')
