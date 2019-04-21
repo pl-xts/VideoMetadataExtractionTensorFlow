b3260548866a73b60f89612e4c9ed61864135fb7
pl-xts <Blefor93@gmail.com>
2019-04-21 19:58:38 +0300

    round average prediction scores

--- tensorflow_hub/utils/store_results.py
+++ tensorflow_hub/utils/store_results.py
@@ -3,7 +3,7 @@ file = open(path,'a+')
 
 
 def store_results(name, num_of_classes, avg_score, time, video_type):
-    string = "{};{};{};{};{}\n".format(name, num_of_classes, avg_score, time, video_type)
+    string = "{};{};{};{};{}\n".format(name, num_of_classes, round(avg_score, 2), time, video_type)
     file.write(string)
 
 # store_results('resnet', 100, 55, "155", "night")
