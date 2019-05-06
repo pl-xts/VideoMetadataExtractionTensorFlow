88f0026e235f119ba791c2745edf268f83a1f7a2
pl-xts <Blefor93@gmail.com>
2019-05-06 14:42:26 +0300

    format

--- tensorflow_hub/yolo_darkflow_custom.py
+++ tensorflow_hub/yolo_darkflow_custom.py
@@ -38,7 +38,7 @@ do_resize = False
 options = {"model": "./cfg/tiny-yolo-vocCustom.cfg",
            "threshold": 0.1, 
            "gpu": 1.0,
-           "load": 700,}
+           "load": -1,}
 #700, 888, 1332, -1
 start = time.time()
 tfnet = TFNet(options)
@@ -61,8 +61,8 @@ def boxing(original_img, predictions):
         label = result['label'] + " " + str(round(confidence, 3))
 
         if confidence > 0.1:
-            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
-            newImage = cv2.putText(newImage, label, (top_x, top_y+50), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (0, 230, 0), 1, cv2.LINE_AA)
+            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (0,0,255), 3)
+            newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (255, 0, 0), 1, cv2.LINE_AA)
             
     return newImage
 
